32b05e605927cb4efc2d6a3d7686abab3e8c7aef
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:38:14 +0100

    Remove remain unused method

--- _sunamo/SunamoFileSystem/FSND.cs
+++ _sunamo/SunamoFileSystem/FSND.cs
@@ -5,11 +5,4 @@ internal class FSND
 
     
 
-    internal static string FirstCharUpper(string nazevPP)
-    {
-        if (nazevPP.Length == 1) return nazevPP.ToUpper();
-
-        var sb = nazevPP.Substring(1);
-        return nazevPP[0].ToString().ToUpper() + sb;
-    }
 }
