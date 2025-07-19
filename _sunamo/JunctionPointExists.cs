4091680801d41f39894115be1bdbd6db228d01f2
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-19 15:26:48 +0200

    feat: Made internal in _sunamo

--- _sunamo/JunctionPointExists.cs
+++ _sunamo/JunctionPointExists.cs
@@ -1,6 +1,6 @@
 namespace SunamoGetFolders._sunamo;
 
-public partial class JunctionPoint
+internal partial class JunctionPoint
 {
     /// <summary>
     ///     Command to get the reparse point data block.
@@ -21,7 +21,7 @@ public partial class JunctionPoint
     ///     Thrown if the specified path is invalid
     ///     or some other error occurs
     /// </exception>
-    public static bool IsJunctionPoint(ILogger logger, string path)
+    internal static bool IsJunctionPoint(ILogger logger, string path)
     {
         if (!Directory.Exists(path))
             return false;
@@ -185,40 +185,40 @@ Nepomůže spustit ani VS jako admin
         /// <summary>
         ///     Reparse point tag. Must be a Microsoft reparse point tag.
         /// </summary>
-        public uint ReparseTag;
+        internal uint ReparseTag;
         /// <summary>
         ///     Size, in bytes, of the data after the Reserved member. This can be calculated by:
         ///     (4 * sizeof(ushort)) + SubstituteNameLength + PrintNameLength +
         ///     (namesAreNullTerminated ? 2 * sizeof(char) : 0);
         /// </summary>
-        public ushort ReparseDataLength;
+        internal ushort ReparseDataLength;
         /// <summary>
         ///     Reserved; do not use.
         /// </summary>
-        public ushort Reserved;
+        internal ushort Reserved;
         /// <summary>
         ///     Offset, in bytes, of the substitute name string in the PathBuffer array.
         /// </summary>
-        public ushort SubstituteNameOffset;
+        internal ushort SubstituteNameOffset;
         /// <summary>
         ///     Length, in bytes, of the substitute name string. If this string is null-terminated,
         ///     SubstituteNameLength does not include space for the null character.
         /// </summary>
-        public ushort SubstituteNameLength;
+        internal ushort SubstituteNameLength;
         /// <summary>
         ///     Offset, in bytes, of the print name string in the PathBuffer array.
         /// </summary>
-        public ushort PrintNameOffset;
+        internal ushort PrintNameOffset;
         /// <summary>
         ///     Length, in bytes, of the print name string. If this string is null-terminated,
         ///     PrintNameLength does not include space for the null character.
         /// </summary>
-        public ushort PrintNameLength;
+        internal ushort PrintNameLength;
         /// <summary>
         ///     A buffer containing the unicode-encoded path string. The path string contains
         ///     the substitute name string and print name string.
         /// </summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x3FF0)]
-        public byte[] PathBuffer;
+        internal byte[] PathBuffer;
     }
 }
